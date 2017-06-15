0e63799ec4e7af1aa624eb5603bbfbc15fb17597
Gregor Volkmann <mail@volkmann.me>
2017-06-15 10:02:05 +0200

    shadow_fix

--- raytracer.py
+++ raytracer.py
@@ -5,6 +5,8 @@ from PIL import Image
 from objects import *
 from misc import *
 
+TOLERANCE = 5e-16
+
 image_width = 400
 image_height = 400
 image = Image.new('RGB', (image_width, image_height))
@@ -46,27 +48,28 @@ class Raytracer:
                                 lr = l.mirror(n)
                                 light_ray = Ray(p, l)
                                 light_dist = (light - p).length()
-                                # ambient
                                 ca = object.colorAt(ray)
+                                # ambient
                                 ka = object.material.ambient_coefficient
                                 c_ambient = ca * ka
-
-                                cin = light.color
-                                # shadow
-                                for object in filter(lambda x: not isinstance(x, Light) and not x is object, self.object_list):
-                                    object_dist = object.intersectionParameter(light_ray)
-                                    if object_dist and object_dist > 5e-16:
-                                        cin = object.colorAt(light_ray).intensity()
-
                                 # diffuse
+                                cin = light.color
                                 kd = object.material.diffuse_coefficient
                                 cos_fi = l.dot(n)
                                 c_diffuse = cin * kd * cos_fi
                                 # specular
+                                cin = light.color
                                 ks = object.material.specular_coefficient
                                 cos_0_n = (lr.dot(d*-1))**object.material.roughness
                                 c_specular = cin * ks * cos_0_n
 
+                                # shadow
+                                for object in filter(lambda x: not isinstance(x, Light) and not x is object, self.object_list):
+                                    object_dist = object.intersectionParameter(light_ray)
+                                    if object_dist and object_dist > TOLERANCE:
+                                        c_ambient *= 0.75
+                                        c_diffuse *= 0.75
+                                        c_specular = Color(0, 0, 0)
                                 color = c_ambient + c_diffuse + c_specular
                 image.putpixel((x, y), color.rgb())
         return image.rotate(180)
