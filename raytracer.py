8b2185e066666d336f8c97273a7722b9d7e8fcdf
Gregor Volkmann <mail@volkmann.me>
2017-06-15 09:59:47 +0200

    light_beta

--- raytracer.py
+++ raytracer.py
@@ -27,12 +27,28 @@ class Raytracer:
                 ray = self.calc_ray(x, y)
                 maxdist = float('inf')
                 color = BACKGROUND_COLOR
-                for object in self.object_list:
+                for object in filter(lambda x: not isinstance(x, Light), self.object_list):
                     hitdist = object.intersectionParameter(ray)
                     if hitdist:
                         if hitdist < maxdist and hitdist > 0:
                             maxdist = hitdist
+
+                            p = ray.pointAtParameter(maxdist)
+                            n = object.normalAt(p)
+                            d = ray.direction
+                            
                             color = object.colorAt(ray)
+                            for light in filter(lambda x: isinstance(x, Light), self.object_list):
+                                l = (light - p).normalized()
+                                lr = l.mirror(n)
+                                light_ray = Ray(p, l)
+                                # object zwischen Lichtstrahl
+                                for object in filter(lambda x: not isinstance(x, Light), self.object_list):
+                                    light_hitdist = object.intersectionParameter(light_ray)
+                                    if light_hitdist:
+                                        if light_hitdist > 0:
+                                            color = Color(0, 0, 0)
+
                 image.putpixel((x, y), color.rgb())
         return image.rotate(180)
 
@@ -48,21 +64,23 @@ if __name__ == '__main__':
 
     tracer = Raytracer(camera)
 
-    plane = Plane(Point(0, 0, 0), Vector(0, 1, 0))
+    plane = Plane(Point(0, 0, 0), Vector(0, 1, 0), Color(125, 125, 125))
     sphere_red = Sphere(Point(1.5, 2, 0), 1, Color(255, 0, 0))
     sphere_green = Sphere(Point(-1.5, 2, 0), 1, Color(0, 255, 0))
     sphere_blue = Sphere(Point(0, 4.5, 0), 1, Color(0, 0, 255))
     triangle = Triangle(Point(1.5, 2, 0), Point(-1.5, 2, 0), Point(0, 4.5, 0), Color(255, 255, 0))
+    light = PointLight(30, 30, 10, 1)
 
     tracer.addObject(plane)
     tracer.addObject(sphere_red)
     tracer.addObject(sphere_green)
     tracer.addObject(sphere_blue)
     tracer.addObject(triangle)
+    tracer.addObject(light)
 
     for object in tracer.object_list:
         print(object)
 
     img = tracer.render_image()
     img.show()
-    # img.save('img.jpg', 'JPEG')
+    img.save('img.jpg', 'JPEG')
