7e3eda0ec8b6d404f878a5e3dd119a55bc366094
Gregor Volkmann <mail@volkmann.me>
2017-06-15 10:02:52 +0200

    spec(tac)ular + refactored

--- misc.py
+++ misc.py
@@ -1,6 +1,62 @@
 # -*- coding: utf-8 -*-
 
-from objects import Vector
+import math
+
+class Point(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return 'Point(%s,%s,%s)' % (self.x, self.y, self.z)
+
+    def __add__(self, vector):
+        return Point(self.x+vector.x, self.y+vector.y, self.z+vector.z)
+
+    def __sub__(self, point):
+        return Vector(self.x-point.x, self.y-point.y, self.z-point.z)
+
+class Vector(Point):
+    def __repr__(self):
+        return 'Vector(%s,%s,%s)' % (self.x, self.y, self.z)
+
+    # http://www.mathe-online.at/materialien/ursl/files/Rechnen.html
+    def __add__(self, vector):
+        return Vector(self.x+vector.x, self.y+vector.y, self.z+vector.z)
+
+    def __mul__(self, n):
+        return Vector(self.x*n, self.y*n, self.z*n)
+
+    # http://www.lernort-mint.de/Mathematik/Vektoren/vektoren_division.html
+    def __div__(self, n):
+        return Vector(self.x/n, self.y/n, self.z/n)
+
+    # skalarprodukt ⟨⟩
+    def dot(self, v):
+        return self.x*v.x + self.y*v.y + self.z*v.z
+
+    def length(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    # http://massmatics.de/merkzettel/index.php#!339:Vektoren_normieren
+    def normalized(self):
+        return Vector(self.x/self.length(), self.y/self.length(), self.z/self.length())
+
+    def scale(self, t):
+        return Vector(self.x*t, self.y*t, self.z*t)
+
+    # https://www.youtube.com/watch?v=UzWnp97GN9g
+    def cross(self, v):
+        return Vector(
+            self.y*v.z - self.z*v.y,
+            self.z*v.x - self.x*v.z,
+            self.x*v.y - self.y*v.x,
+        )
+
+    # Script Seite 48
+    def mirror(self, v):
+        return self-v*self.dot(v)*2
 
 class Color(object):
     def __init__(self, r, g, b):
@@ -33,7 +89,7 @@ class Material(object):
         return self.base_color
 
 class CheckerboardMaterial(Material):
-    def __init__(self, base_color, ambient_coefficient, diffuse_coefficient, specular_coefficient, other_color, checkSize):
+    def __init__(self, base_color, ambient_coefficient, diffuse_coefficient, specular_coefficient, roughness, other_color, checkSize):
         Material.__init__(self, base_color, ambient_coefficient, diffuse_coefficient, specular_coefficient)
         self.other_color = other_color
         self.checkSize = checkSize
--- raytracer.py
+++ raytracer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Code by Gregor Volkmann
+
 from PIL import Image
 from objects import *
 from misc import *
@@ -11,6 +13,7 @@ image_width = 400
 image_height = 400
 image = Image.new('RGB', (image_width, image_height))
 BACKGROUND_COLOR = Color(0, 0, 0)
+SHADOW_CONST = 0.25
 
 class Raytracer:
     def __init__(self, camera):
@@ -42,14 +45,13 @@ class Raytracer:
                             color = object.colorAt(ray)
 
                             # CALC LIGHT
-                            light_maxdist = float('inf')
                             for light in filter(lambda x: isinstance(x, Light), self.object_list):
                                 l = (light - p).normalized()
                                 lr = l.mirror(n)
                                 light_ray = Ray(p, l)
-                                light_dist = (light - p).length()
-                                ca = object.colorAt(ray)
+
                                 # ambient
+                                ca = object.colorAt(ray)
                                 ka = object.material.ambient_coefficient
                                 c_ambient = ca * ka
                                 # diffuse
@@ -58,18 +60,24 @@ class Raytracer:
                                 cos_fi = l.dot(n)
                                 c_diffuse = cin * kd * cos_fi
                                 # specular
-                                cin = light.color
+                                if math.degrees(math.acos(light_ray.direction.dot(n))) > 90.0: # Strahl zeigt nach innen
+                                    cin = Color(0, 0, 0)
+                                else:
+                                    cin = light.color
                                 ks = object.material.specular_coefficient
                                 cos_0_n = (lr.dot(d*-1))**object.material.roughness
                                 c_specular = cin * ks * cos_0_n
 
                                 # shadow
+                                object_maxdist = float('inf')
                                 for object in filter(lambda x: not isinstance(x, Light) and not x is object, self.object_list):
                                     object_dist = object.intersectionParameter(light_ray)
-                                    if object_dist and object_dist > TOLERANCE:
-                                        c_ambient *= 0.75
-                                        c_diffuse *= 0.75
-                                        c_specular = Color(0, 0, 0)
+                                    if object_dist:
+                                        if object_dist < object_maxdist and object_dist > TOLERANCE:
+                                            object_maxdist = object_dist
+                                            c_ambient *= SHADOW_CONST
+                                            c_diffuse *= SHADOW_CONST
+                                            c_specular = Color(0, 0, 0)
                                 color = c_ambient + c_diffuse + c_specular
                 image.putpixel((x, y), color.rgb())
         return image.rotate(180)
@@ -86,10 +94,10 @@ if __name__ == '__main__':
 
     tracer = Raytracer(camera)
 
-    plane = Plane(Point(0, 0, 0), Vector(0, 1, 0), CheckerboardMaterial(Color(255, 255, 255), 1.0, 0.6, 0.2, Color(0, 0, 0), 1))
-    sphere_red = Sphere(Point(1.5, 2, 0), 1,  Material(Color(255, 0, 0), 1.0, 0.6, 0.2))
-    sphere_green = Sphere(Point(-1.5, 2, 0), 1,  Material(Color(0, 255, 0), 1.0, 0.6, 0.2))
-    sphere_blue = Sphere(Point(0, 4.5, 0), 1,  Material(Color(0, 0, 255), 1.0, 0.6, 0.2))
+    plane = Plane(Point(0, 0, 0), Vector(0, 1, 0), CheckerboardMaterial(Color(255, 255, 255), 1.0, 0.6, 0.2, 40, Color(0, 0, 0), 1))
+    sphere_red = Sphere(Point(1.5, 2, 0), 1,  Material(Color(255, 0, 0), 1.0, 0.6, 0.0, 7))
+    sphere_green = Sphere(Point(-1.5, 2, 0), 1,  Material(Color(0, 255, 0), 1.0, 0.6, 0.5))
+    sphere_blue = Sphere(Point(0, 4.5, 0), 1,  Material(Color(0, 0, 255), 1.0, 0.6, 0.2, 32))
     triangle = Triangle(Point(1.5, 2, 0), Point(-1.5, 2, 0), Point(0, 4.5, 0),  Material(Color(255, 255, 0), 1.0, 0.6, 0.2))
     light = PointLight(30, 30, 10, Color(255, 255, 255))
 
