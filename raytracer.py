522e553956e2cd0e6e32e782564e770e608e39cf
Gregor Volkmann <mail@volkmann.me>
2017-06-15 10:03:16 +0200

    reshadow

--- raytracer.py
+++ raytracer.py
@@ -138,7 +138,51 @@ class Raytracer:
         return directColor + reflectColor*REFLECTION # + refraction*refractedColor
 
     def computeDirectLight(self, hitPointData):
-        return hitPointData['object'].colorAt(hitPointData['ray'])
+        # return hitPointData['object'].colorAt(hitPointData['ray'])
+
+
+        p = hitPointData['p']
+        object = hitPointData['object']
+        n = hitPointData['object'].normalAt(hitPointData['p'])
+        ray = hitPointData['ray']
+        d = self.computeReflectedRay(hitPointData).direction
+
+        # CALC LIGHT
+        for light in filter(lambda x: isinstance(x, Light), self.object_list):
+            l = (light - p).normalized()
+            lr = l.mirror(n)
+            light_ray = Ray(p, l)
+
+            # ambient
+            ca = object.colorAt(ray)
+            ka = object.material.ambient_coefficient
+            c_ambient = ca * ka
+            # diffuse
+            cin = light.color
+            kd = object.material.diffuse_coefficient
+            cos_fi = l.dot(n)
+            c_diffuse = cin * kd * cos_fi
+            # specular
+            if math.degrees(math.acos(light_ray.direction.dot(n))) > 90.0: # Strahl zeigt nach innen
+                cin = Color(0, 0, 0)
+            else:
+                cin = light.color
+            ks = object.material.specular_coefficient
+            cos_0_n = (lr.dot(d*-1))**object.material.roughness
+            c_specular = cin * ks * cos_0_n
+
+            # shadow
+            object_maxdist = float('inf')
+            for object in filter(lambda x: not isinstance(x, Light) and not x is object, self.object_list):
+                object_dist = object.intersectionParameter(light_ray)
+                if object_dist:
+                    if object_dist < object_maxdist and object_dist > TOLERANCE:
+                        object_maxdist = object_dist
+                        c_ambient *= SHADOW_CONST
+                        c_diffuse *= SHADOW_CONST
+                        c_specular = Color(0, 0, 0)
+            color = c_ambient + c_diffuse + c_specular
+        return color
 
     def computeReflectedRay(self, hitPointData):
         return Ray(hitPointData['p'], hitPointData['ray'].direction.mirror(hitPointData['object'].normalAt(hitPointData['p'])))
