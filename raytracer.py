31a6b3ec157d6f8565a971fdac04b7bc87230020
Gregor Volkmann <mail@volkmann.me>
2017-06-24 21:52:44 +0200

    refactor non recursive raytracing

--- raytracer.py
+++ raytracer.py
@@ -36,33 +36,32 @@ class Raytracer(object):
                 ray = self.calc_ray(x, y)
                 maxdist = float('inf')
                 color = BACKGROUND_COLOR
-                # objectception wanted by codacy
-                # for object in filter(lambda x: not isinstance(x, Light), self.object_list):
-                for object in [object for object in self.object_list if not isinstance(object, Light)]:
-                    hitdist = object.intersectionParameter(ray)
+                # aggregate object inside renderdistance
+                for intersection_object in [intersection_object for intersection_object in self.object_list if not isinstance(intersection_object, Light)]:
+                    hitdist = intersection_object.intersectionParameter(ray)
                     if hitdist:
                         if hitdist < maxdist and hitdist > 0:
                             maxdist = hitdist
 
                             p = ray.pointAtParameter(maxdist)
-                            n = object.normalAt(p)
+                            n = intersection_object.normalAt(p)
                             d = ray.direction
 
-                            color = object.colorAt(ray)
+                            color = intersection_object.colorAt(ray)
 
-                            # CALC LIGHT
+                            # calculate light
                             for light in filter(lambda x: isinstance(x, Light), self.object_list):
                                 l = (light - p).normalized()
                                 lr = l.mirror(n)
                                 light_ray = Ray(p, l)
 
                                 # ambient
-                                ca = object.colorAt(ray)
-                                ka = object.material.ambient_coefficient
+                                ca = intersection_object.colorAt(ray)
+                                ka = intersection_object.material.ambient_coefficient
                                 c_ambient = ca * ka
                                 # diffuse
                                 cin = light.color
-                                kd = object.material.diffuse_coefficient
+                                kd = intersection_object.material.diffuse_coefficient
                                 cos_fi = l.dot(n)
                                 c_diffuse = cin * kd * cos_fi
                                 # specular
@@ -70,14 +69,14 @@ class Raytracer(object):
                                     cin = Color(0, 0, 0)
                                 else:
                                     cin = light.color
-                                ks = object.material.specular_coefficient
-                                cos_0_n = (lr.dot(d*-1))**object.material.roughness
+                                ks = intersection_object.material.specular_coefficient
+                                cos_0_n = (lr.dot(d*-1))**intersection_object.material.roughness
                                 c_specular = cin * ks * cos_0_n
 
-                                # shadow
+                                # calculate shadow
                                 object_maxdist = float('inf')
-                                for object in filter(lambda x: not isinstance(x, Light) and not x is object, self.object_list):
-                                    object_dist = object.intersectionParameter(light_ray)
+                                for shadow_object in filter(lambda x: not isinstance(x, Light) and not x is intersection_object, self.object_list):
+                                    object_dist = shadow_object.intersectionParameter(light_ray)
                                     if object_dist:
                                         if object_dist < object_maxdist and object_dist > TOLERANCE:
                                             object_maxdist = object_dist
